bd156a23d412b59175e3e86cfe2aede35c45eca0
hp1 <hp1@10.1.165.48>
2014-02-28 11:49:01 +0200

    First Commit.

---
+++ src/com/facebookanalizapp/process/Node.java
@@ -0,0 +1,50 @@
+
+package com.facebookanalizapp.process;
+
+/**
+ *
+ * @author ufuk halis
+ */
+public class Node {
+    private String name;
+    private Data data;
+    private Mining mining;
+    private Presentation presentation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    public void addData(String databaseName){
+        
+    }
+    
+    public void addPresentation(String presentationName){
+        
+    }
+    
+    public void addMining(String miningName){
+        
+    }
+    
+    public void removeData(){
+        
+    }
+    
+    public void removePresentation(){
+        
+    }
+    
+    public void removeMining(){
+        
+    }
+    
+    public void execute(){
+        
+    }
+}
